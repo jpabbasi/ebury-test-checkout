b502e6712b0ca1e420957306e1edb7824998037c
jpabbasi <joao.abbasi@gmail.com>
2024-09-22 22:42:05 -0300

    🚀 added main application to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardForm from './components/CardForm';
+import { handleFormSubmit } from './services/submitHandlers';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardForm onSubmit={handleFormSubmit} />
     </div>
   );
-}
+};
 
 export default App;
